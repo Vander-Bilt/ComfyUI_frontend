00bf8be60661a76c16bf782945a6757fe9c1ae65
bilt <vanderbilt@gmail.com>
2025-07-24 20:51:46 +0800

    51la & DonatePanel

--- src/components/dialog/content/setting/DonatePanel.vue
+++ src/components/dialog/content/setting/DonatePanel.vue
@@ -6,19 +6,22 @@
     <p class="mb-4">{{ $t('g.donateMessage1') }}</p>
     <p class="mb-4">{{ $t('g.donateMessage2') }}</p>
 
-    <div class="flex gap-4">
-      <div>
-        <h3 class="text-lg font-semibold mb-2">Alipay</h3>
-        
+    <div class="mt-4">
+
+      <div v-if="locale === 'zh'">
+        <iframe id='pay_zh' src='https://donate.nav001.online/index_zh.html' style='border:none;width:100%;padding:0px;background:#f9f9f9;' height='600' title='vanderbilt'></iframe>
       </div>
-      <div>
-        <h3 class="text-lg font-semibold mb-2">WeChat Pay</h3>
-        
+      <div v-else>
+        <iframe id='kofiframe' src='https://ko-fi.com/vanderbilt/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='600' title='vanderbilt'></iframe>
       </div>
+
     </div>
   </PanelTemplate>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 import PanelTemplate from './PanelTemplate.vue'
+
+const { locale } = useI18n()
 </script>
