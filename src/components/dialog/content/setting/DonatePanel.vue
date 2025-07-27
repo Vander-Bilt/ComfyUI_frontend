f9e132ff0e30937e0dcfdd0763d66c3a63f9ac4b
bilt <vanderbilt@gmail.com>
2025-07-27 12:03:04 +0800

    donate light

--- src/components/dialog/content/setting/DonatePanel.vue
+++ src/components/dialog/content/setting/DonatePanel.vue
@@ -9,7 +9,7 @@
     <div class="mt-4">
 
       <div v-if="locale === 'zh'">
-        <iframe id='pay_zh' src='https://donate.nav001.online/index_zh.html' style='border:none;width:100%;padding:0px;background:#f9f9f9;' height='600' title='vanderbilt'></iframe>
+        <iframe id='pay_zh' :src='donateUrl' style='border:none;width:100%;padding:0px;background:#f9f9f9;' height='600' title='vanderbilt'></iframe>
       </div>
       <div v-else>
         <iframe id='kofiframe' src='https://ko-fi.com/vanderbilt/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='600' title='vanderbilt'></iframe>
@@ -20,8 +20,22 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import PanelTemplate from './PanelTemplate.vue'
+import { useColorPaletteStore } from '@/stores/workspace/colorPaletteStore'
 
 const { locale } = useI18n()
+const colorPaletteStore = useColorPaletteStore()
+
+const theme = computed(() =>
+  colorPaletteStore.completedActivePalette.light_theme ? 'light' : 'dark'
+)
+
+const donateUrl = computed(() => {
+  const baseUrl = 'https://donate.nav001.online/index_zh.html'
+  const url = new URL(baseUrl)
+  url.searchParams.append('theme', theme.value)
+  return url.toString()
+})
 </script>
